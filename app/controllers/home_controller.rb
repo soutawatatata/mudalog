fdba5f0184738677add7b75d0b8da1af45b6703f
Mac User <macuser@Mac-no-MacBook-Air-2.local>
2018-08-30 20:42:24 +0900

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    if @post.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("home/top")
+    end
+  end
+end
